812a448ee7b911bc47bd73dfb5ce720de0986803
rsternik <rsternik.wg@gmail.com>
2022-01-18 19:37:39 -0800

    port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid')
 const db = './db/db.json'
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 const app = express();
 const dataRes = require("./db/db.json")
 
