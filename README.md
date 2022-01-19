b33068ec783cb499ea4fd2391d6ae9e8d8d7c47f
rsternik <rsternik.wg@gmail.com>
2022-01-18 17:40:08 -0800

    comments added for readablity, some formatting changes

---
+++ README.md
@@ -0,0 +1,21 @@
+# Note Taker
+  
+  ## Table of Contents
+  
+1. [Description](#description)
+2. [Installation](#installation)
+3. [License](#test)
+4. [Github](#github)
+
+
+## Description
+Application to write and save notes
+## Installation
+Requires NPM install express and uuid
+## License
+Unlicensed.
+## Github
+Checkout my github here  https://github.com/rsternik
+## Contact
+If you have any questions please email me at rsternik.wg@gmail.com
+
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
+// Required resources
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid')
 const db = './db/db.json'
-
 const PORT = 3001;
-
 const app = express();
-
 const dataRes = require("./db/db.json")
+
+// Express app use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-
+// default get route to index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+// route to notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
@@ -28,22 +28,25 @@ app.listen(PORT, () =>
 );
 
 app.get('/api/notes', (req, res) => res.json(dataRes));
-
+// Post route 
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method} received for notes!}`)
 
     const { title, text } = req.body
-
+    // condition if data is enter in post route
     if (req.body) {
         const newNote = {
             title,
             text,
             id: uuidv4(),
         }
+        // read db.json
         fs.readFile(db, 'utf8', (err, data) => {
             if (err) {
                 console.log(err)
-            } else {
+            } 
+            // write post to db.json
+            else {
                 const parsedData = JSON.parse(data)
                 parsedData.push(newNote)
                 res.json(parsedData)
